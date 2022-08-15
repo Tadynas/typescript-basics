302bee7af0a38276787ca3033b065185a4949dcc
Tadynas <griciustadas@gmail.com>
2022-08-15 15:20:14 +0300

    functions

--- src/index.ts
+++ src/index.ts
@@ -24,3 +24,16 @@ let user: [number, string] = [1, 'Ted']
 const enum Size { Small = 1, Medium, Large }
 let mySize: Size = Size.Medium
 console.log(mySize)
+
+
+
+// Functions
+function calculateTax(income?: number, taxYear: number = 2022): number {
+    if(taxYear < 2022) {
+        return (income || 50_000) * 1.2
+    } else {
+        return (income || 50_000) * 1.3
+    }
+}
+
+calculateTax(10_000)

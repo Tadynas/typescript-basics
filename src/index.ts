64a9bb17f6b0428d89a0a561d4689f4258201b0c
Tadynas <griciustadas@gmail.com>
2022-08-15 15:47:39 +0300

    Optional chaining

--- src/index.ts
+++ src/index.ts
@@ -111,3 +111,23 @@ function greet(name: string | null | undefined) {
 }
 
 greet(null)
+
+// Optional chaining
+type Customer = {
+    birthday?: Date
+}
+
+function getCustomer(id: number): Customer | null | undefined {
+    return id === 0 ? null : { birthday: new Date() }
+}
+
+let customer = getCustomer(0)
+// Optional property access operator
+console.log(customer?.birthday?.getFullYear())
+
+// Optional element access operator
+// customers?.[0]
+
+// Optional call
+let log: any = null
+log?.('a')

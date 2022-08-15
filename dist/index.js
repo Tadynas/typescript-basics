3da4a027367eb604fa4dcae986f4ce45e5c244f7
Tadynas <griciustadas@gmail.com>
2022-08-15 15:09:03 +0300

    tuples

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,13 @@
 "use strict";
+let sales = 123456789;
+let course = 'TypeScript';
+let is_published = true;
+let level;
+function render(document) {
+    console.log(document);
+}
+let numbers = [1, 2, 3];
+let anyNumber = [];
+anyNumber[0] = 1;
+anyNumber[1] = '1';
+let user = [1, 'Ted'];
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
+// Types
 let sales: number = 123_456_789
 let course: string = 'TypeScript'
 let is_published: boolean = true
-let level
 
+// Any
+let level
 function render(document: any) {
     console.log(document)
 }
 
+// Arrays
 let numbers: number[] = [1, 2, 3]
 
 let anyNumber = []
 anyNumber[0] = 1
 anyNumber[1] = '1'
+
+// Tuples
+let user: [number, string] = [1, 'Ted']
